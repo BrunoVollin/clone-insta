2add3b18ff4017180fb01a85e12ca2811db377bd
bruno <bruno.3av@gmail.com>
2021-03-09 20:54:19 -0300

    added models

---
+++ src/Models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+      type: String,
+      require: true 
+  }
+});
+
+module.exports = mongoose.model("User", Schema);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = require("./Utils/router");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const mongoConnection = process.env.MONGO_URI;
 
 const app = express();
 
-app.use(express.json())
-app.use(router)
+mongoose.connect(mongoConnection, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}, () => console.log("connected"));
 
-app.listen(3333, () => console.log("servidor rodando na porta 3333"))
+app.use(express.json());
+app.use(router);
+
+app.listen(3333, () => console.log("servidor rodando na porta 3333"));
